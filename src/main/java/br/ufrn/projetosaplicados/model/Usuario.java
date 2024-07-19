02b5f377175b32425c6e2df279449b8beed3bfa6
Phelyppe Álex <phelyppesilva@gmail.com>
2024-07-19 11:14:31 -0300

    email tem que ser único

--- src/main/java/br/ufrn/projetosaplicados/model/Usuario.java
+++ src/main/java/br/ufrn/projetosaplicados/model/Usuario.java
@@ -1,5 +1,6 @@
 package br.ufrn.projetosaplicados.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
 }
